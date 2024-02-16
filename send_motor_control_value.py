59aab5a1cc3b0c775dff06685a0ad9b1b2a438f6
SHIMA Naoki <shima@numazu-ct.ac.jp>
2024-02-16 18:18:53 +0900

    Fix エンコーディングを追加

--- send_motor_control_value.py
+++ send_motor_control_value.py
@@ -17,7 +17,7 @@ def send_motor_control_amount(left=0.0, right=0.0, duration=1.0):
     # メッセージの組み立て
     msg_txt = f"{left:.1f},{right:.1f},{duration:2.1f}"
     # https://python-can.readthedocs.io/en/stable/message.html
-    msg_bytes = bytes(msg_txt)
+    msg_bytes = bytes(msg_txt, 'utf-8')
     msg = can.Message(data=msg_bytes)
 
     try:
